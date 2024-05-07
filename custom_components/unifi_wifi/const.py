ef4ef2e0a28a8faff2c3c1933e8a82f679a7b8b3
John Stenger <rootnegativ1@users.noreply.github.com>
2024-05-07 05:40:32 -0700

    update constant
    change ```UNIFI_PASSPHRASE``` to ```UNIFI_X_PASSPHRASE``` to maintain naming consistency

--- custom_components/unifi_wifi/const.py
+++ custom_components/unifi_wifi/const.py
@@ -33,6 +33,6 @@ CONF_WPA3_TRANSITION = 'wpa3_transition'
 UNIFI_ID = '_id'
 UNIFI_NAME = 'name' # duplicate
 UNIFI_NETWORKCONF_ID = 'networkconf_id'
-UNIFI_PASSPHRASE = 'x_passphrase'
+UNIFI_X_PASSPHRASE = 'x_passphrase'
 UNIFI_PASSWORD = 'password' # duplicate
 UNIFI_PRESHARED_KEYS = 'private_preshared_keys'
