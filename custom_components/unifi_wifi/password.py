db6fe400ba5e95d701d3a5259ea127978e55104d
John Stenger <rootnegativ1@users.noreply.github.com>
2025-01-02 17:02:48 -0800

    add new method for password generation

--- custom_components/unifi_wifi/password.py
+++ custom_components/unifi_wifi/password.py
@@ -5,6 +5,8 @@ import secrets, string
 from xkcdpass import xkcd_password as xp
 
 WORD_FILE = '/config/custom_components/unifi_wifi/eff_large_wordlist.txt'
+COLOR_FILE= '/config/custom_components/unifi_wifi/color_wordlist.txt'
+NOUN_FILE = '/config/custom_components/unifi_wifi/noun_wordlist.txt'
 
 
 def create(_method: str, _delimiter: str, _min_length: int, _max_length: int, _word_count: int, _char_count: int):
@@ -14,26 +16,30 @@ def create(_method: str, _delimiter: str, _min_length: int, _max_length: int, _w
         # however this is not available to the function so we specify a local copy of eff_long
         # this file is located in the current working directory
         mywords = xp.generate_wordlist(wordfile=WORD_FILE, min_length=_min_length, max_length=_max_length)
-        # x = xp.generate_xkcdpassword(mywords, numwords=_word_count, delimiter=' ')
         x = xp.generate_xkcdpassword(mywords, numwords=_word_count, delimiter=_delimiter)
 
-    # https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
-    # this is basically the same as xkcd method
-    # but without extra specificity such as min and max lengths
-    # On standard Linux systems, use a convenient dictionary file.
-    # Other platforms may need to provide their own word-list.
+    # this is basically the same as xkcd method but without extra specificity such as min and max lengths
+    # On standard Linux systems, use a convenient dictionary file. Other platforms may need to provide their own word-list.
     # https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
     elif _method == 'word':
         with open(WORD_FILE) as f:
             words = [word.strip() for word in f]
-            # x = ' '.join(secrets.choice(words) for i in range(_word_count))
             x = _delimiter.join(secrets.choice(words) for i in range(_word_count))
 
     # https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
-    # 'char' method
-    elif _method =='char':
+    elif _method == 'char':
         alphabet = string.ascii_letters + string.digits
         x = ''.join(secrets.choice(alphabet) for i in range(_char_count))
+
+    elif _method == 'rainbow':
+        with open(COLOR_FILE) as f:
+            color = secrets.choice([c.strip() for c in f])
+        with open(NOUN_FILE) as f:
+            noun = secrets.choice([n.strip() for n in f])
+        salt = ''.join(secrets.choice(string.digits) for i in range(5))
+        x = string.capwords(color) + string.capwords(noun) + salt
+
     else:
         raise ValueError(f"Method {_method} is not a valid option.")
+
     return x
